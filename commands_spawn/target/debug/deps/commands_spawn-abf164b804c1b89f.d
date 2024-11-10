65191fa5ed478e2c86eb5c59289e6acb66e0ebe2
xubuntu <xubuntu@gmail.com>
2024-11-10 22:01:38 +0000

    Run command capture

--- commands_spawn/target/debug/deps/commands_spawn-abf164b804c1b89f.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/repos/rust/commands_spawn/target/debug/deps/libcommands_spawn-abf164b804c1b89f.rmeta: src/main.rs
-
-/home/ubuntu/repos/rust/commands_spawn/target/debug/deps/commands_spawn-abf164b804c1b89f.d: src/main.rs
-
-src/main.rs:
--- commands_spawn/target/debug/deps/commands_spawn-c0a93a173722474a.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/repos/rust/commands_spawn/target/debug/deps/libcommands_spawn-c0a93a173722474a.rmeta: src/main.rs
-
-/home/ubuntu/repos/rust/commands_spawn/target/debug/deps/commands_spawn-c0a93a173722474a.d: src/main.rs
-
-src/main.rs:
