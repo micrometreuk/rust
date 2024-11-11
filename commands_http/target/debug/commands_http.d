4afca7d9336b70336240283413a9ecc5b878a973
xubuntu <xubuntu@gmail.com>
2024-11-11 19:24:18 +0000

    add rus

---
+++ commands_http/target/debug/commands_http.d
@@ -0,0 +1,1 @@
+/home/ubuntu/repos/rust/commands_http/target/debug/commands_http: /home/ubuntu/repos/rust/commands_http/src/main.rs
---
+++ commands_http/target/debug/deps/commands_http-d931da188feb9251.d
@@ -0,0 +1,5 @@
+/home/ubuntu/repos/rust/commands_http/target/debug/deps/commands_http-d931da188feb9251: src/main.rs
+
+/home/ubuntu/repos/rust/commands_http/target/debug/deps/commands_http-d931da188feb9251.d: src/main.rs
+
+src/main.rs:
