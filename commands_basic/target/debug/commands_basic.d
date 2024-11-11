e4204c0abb4d7054b0bffa920b17c3cf593152ea
xubuntu <xubuntu@gmail.com>
2024-11-11 08:13:10 +0000

    added commands_basic/

--- commands_basic/target/debug/commands_basic.d
+++
@@ -1,1 +0,0 @@
-/home/ubuntu/repos/rust/commands_basic/target/debug/commands_basic: /home/ubuntu/repos/rust/commands_basic/src/main.rs
--- commands_basic/target/debug/deps/commands_basic-07928dcd72443e06.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/repos/rust/commands_basic/target/debug/deps/commands_basic-07928dcd72443e06: src/main.rs
-
-/home/ubuntu/repos/rust/commands_basic/target/debug/deps/commands_basic-07928dcd72443e06.d: src/main.rs
-
-src/main.rs:
