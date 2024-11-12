84c792900cecc39749d6c268c7e79f086b96e4ea
xubuntu <xubuntu@gmail.com>
2024-11-12 01:57:59 +0000

    adding multu

--- multi_threaded_server/src/main.rs
+++ multi_threaded_server/src/main.rs
@@ -1,3 +1,4 @@
+
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -5,25 +6,25 @@ use std::{
     thread,
     time::Duration,
 };
-use multi_threaded_server::ThreadPool;
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    let pool = ThreadPool::new(4);
-
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
-            handle_connection(stream);
-        });
 
+        handle_connection(stream);
     }
 }
+
+
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+
+
+
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
@@ -37,7 +38,8 @@ fn handle_connection(mut stream: TcpStream) {
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
