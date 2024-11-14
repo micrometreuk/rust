3071edc16a187e37fa228b02280b6ce6a3d3bc92
xubuntu <xubuntu@gmail.com>
2024-11-14 18:56:51 +0000

    single_threaded_server/

--- multi_threaded_server/src/main.rs
+++ multi_threaded_server/src/main.rs
@@ -1,38 +1,36 @@
 use std::{
-    fs, io::prelude::*, net::{TcpListener, TcpStream}
+    env::consts,
+    fmt::format,
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
 };
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("main connected");
+
         handle_connection(stream);
     }
 }
 
-
-
-// --snip--
-
 fn handle_connection(mut stream: TcpStream) {
-    // --snip--
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-
-    let (status_line, filename) = if buffer.starts_with(b"get /http/1.1\r\n") {
+    let (status_line, filename) = if buffer.starts_with(b"GET / HTTP/1.1\r\n") {
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
-
     let contents = fs::read_to_string(filename).unwrap();
-    
-    
-    let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
